3fca4fec0396de0b7ecc9613a64a4d478407b0de
Remy Lian <137064504+remylian@users.noreply.github.com>
2026-04-01 16:34:49 +0200

    changed base path to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Portfolio/",
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: "/",
+  plugins: [react(), tailwindcss()],
 });
